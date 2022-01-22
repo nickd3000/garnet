392e76b90a506c628f75321fb15d3e5002018920
Nick Donnelly <nickd3000@hotmail.com>
2022-01-22 01:35:45 +0000

    bumped version

--- src/main/java/com/physmo/garnet/Garnet.java
+++ src/main/java/com/physmo/garnet/Garnet.java
@@ -38,11 +38,14 @@ import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_SCISSOR_TEST;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glEnable;
 import static org.lwjgl.opengl.GL11.glLoadIdentity;
 import static org.lwjgl.opengl.GL11.glMatrixMode;
 import static org.lwjgl.opengl.GL11.glOrtho;
+import static org.lwjgl.opengl.GL11.glScissor;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
@@ -248,5 +251,10 @@ public class Garnet {
 
     public Input getInput() {
         return input;
+    }
+
+    public void setClipWindow(int x, int y, int width, int height) {
+        glScissor(x, y, width, height);
+        glEnable(GL_SCISSOR_TEST);
     }
 }
