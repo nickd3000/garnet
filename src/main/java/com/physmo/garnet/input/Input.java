921eef6b7e66f05872067a6bcab18c6d4af06ab9
Nick Donnelly <nickd3000@hotmail.com>
2023-03-02 00:39:41 +0000

    Cleanup

--- src/main/java/com/physmo/garnet/input/Input.java
+++ src/main/java/com/physmo/garnet/input/Input.java
@@ -33,13 +33,13 @@ public class Input {
         buttonConfigList.add(new ButtonConfig(123, VirtualButton.LEFT));
         buttonConfigList.add(new ButtonConfig(124, VirtualButton.RIGHT));
         buttonConfigList.add(new ButtonConfig(6, VirtualButton.FIRE1));
-        buttonConfigList.add(new ButtonConfig(48, VirtualButton.MENU));
+        buttonConfigList.add(new ButtonConfig(48, VirtualButton.MENU)); // tab
 
         buttonState = new boolean[0xff];
         buttonStatePrev = new boolean[0xff];
 
         garnet.addKeyboardCallback((key, scancode, action, mods) -> {
-            //System.out.println("keyboard handler - key:"+key+ " scancode:" + scancode + "  action:" + action);
+            //System.out.println("keyboard handler - key:" + key + " scancode:" + scancode + "  action:" + action);
 
             // a=0, d=2, space =49 / action 1/0 down/up
 
