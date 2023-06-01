c4b4e5f25985d9d2d58a8238d7e1eedc877e3df6
Nick Donnelly <nickd3000@hotmail.com>
2023-06-01 02:47:32 +0100

    Added paragraph actual height as return value.

--- src/main/java/com/physmo/garnet/ParagraphDrawer.java
+++ src/main/java/com/physmo/garnet/ParagraphDrawer.java
@@ -6,6 +6,11 @@ import com.physmo.garnet.graphics.Graphics;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Draw a paragraph of text that wraps lines according to the
+ * supplied width.
+ * TODO: cache analyzed strings
+ */
 public class ParagraphDrawer {
     DrawableFont font;
     int padY = 0;
@@ -14,46 +19,77 @@ public class ParagraphDrawer {
         this.font = font;
     }
 
-
+    /**
+     * Set how much to pad the vertical space between lines by.
+     *
+     * @param val
+     */
     public void setPadY(int val) {
         padY = val;
     }
 
-    // TODO: cache analyzed strings.
-    public void drawParagraph(Graphics g, String text, int width, int height, int x, int y) {
-        List<WordInfo> wordInfos = analyzeText(font, text);
+    /**
+     * Draw the supplied string to the display, constrained to the supplied dimensions.
+     *
+     * @param graphics The graphics context to draw the paragraph to
+     * @param text     The text to draw
+     * @param width    Width of the window to format the text within
+     * @param height   Height of the window to format text within
+     * @param x        X coordinate of the drawn text
+     * @param y        Y coordinate of the drawn text
+     * @return The total height of the drawn paragraph.
+     */
+    public int drawParagraph(Graphics graphics, String text, int width, int height, int x, int y) {
+        List<WordMetrics> wordInfos = analyzeText(font, text);
 
         int rx = 0; // rolling x
         int ry = 0; // rolling y
 
-        for (WordInfo wordInfo : wordInfos) {
+        for (WordMetrics wordInfo : wordInfos) {
+
+            if (wordIsCarriageReturn(wordInfo.text)) {
+                rx = 0;
+                ry += font.getLineHeight() + padY;
+                continue;
+            }
+
             if (rx + wordInfo.width > width) {
                 rx = 0;
                 ry += font.getLineHeight() + padY;
             }
 
-            font.drawText(g, wordInfo.text, x + rx, y + ry);
+            font.drawText(graphics, wordInfo.text, x + rx, y + ry);
             rx += wordInfo.width;
             rx += font.getSpaceWidth();
         }
 
+        ry += font.getLineHeight() + padY;
+        return ry;
+
     }
 
+    private boolean wordIsCarriageReturn(String str) {
+        if (str.equals(("\n"))) return true;
+        return str.equals(System.lineSeparator());
+    }
 
-    public List<WordInfo> analyzeText(DrawableFont font, String text) {
-        List<WordInfo> words = new ArrayList<>();
+    /*
+        Split string into words and measure the display size of each word.
+     */
+    private List<WordMetrics> analyzeText(DrawableFont font, String text) {
+        List<WordMetrics> words = new ArrayList<>();
         String[] s = text.split(" ");
         for (int i = 0; i < s.length; i++) {
-            words.add(new WordInfo(s[i], font.getStringWidth(s[i])));
+            words.add(new WordMetrics(s[i], font.getStringWidth(s[i])));
         }
         return words;
     }
 
-    class WordInfo {
+    private class WordMetrics {
         String text;
         int width;
 
-        public WordInfo(String _text, int _width) {
+        public WordMetrics(String _text, int _width) {
             text = _text;
             width = _width;
         }
