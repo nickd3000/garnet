5d3dd98d58aca867a9d429f448f00e7105fba4b2
Nick Donnelly <nickd3000@hotmail.com>
2024-10-13 23:01:29 +0100

    Updated ColorSupplierLinear to take a list of colors instead of 2

--- src/main/java/com/physmo/garnet/toolkit/color/ColorSupplierLinear.java
+++ src/main/java/com/physmo/garnet/toolkit/color/ColorSupplierLinear.java
@@ -6,24 +6,37 @@ import com.physmo.garnet.ColorUtils;
 import static com.physmo.garnet.Utils.lerp;
 
 /**
- * A color supplier used to blend two colors.
+ * A color supplier used to blend multiple colors.
  */
 public class ColorSupplierLinear implements ColorSupplier {
-    float[] c1;
-    float[] c2;
+    float[][] cols;
+    int numColors;
 
-    public ColorSupplierLinear(int c1, int c2) {
-        this.c1 = com.physmo.garnet.ColorUtils.rgbaToFloats(c1);
-        this.c2 = com.physmo.garnet.ColorUtils.rgbaToFloats(c2);
+    public ColorSupplierLinear(int[] colors) {
+        numColors = colors.length;
+        cols = new float[numColors][4];
+        for (int i = 0; i < numColors; i++) {
+            cols[i] = ColorUtils.rgbaToFloats(colors[i]);
+        }
     }
+
 
     @Override
     public int getColor(double t) {
+        double span = (double) 1 / (numColors - 1);
+
+        t = t % 1;
+        int i1 = (int) (t / span);
+        int i2 = i1 + 1;
+        if (i2 >= numColors) i2 -= numColors;
+
+        double u = (t % span) / span;
+
         return ColorUtils.asRGBA(
-                lerp(c1[0], c2[0], (float) t),
-                lerp(c1[1], c2[1], (float) t),
-                lerp(c1[2], c2[2], (float) t),
-                lerp(c1[3], c2[3], (float) t));
+                lerp(cols[i1][0], cols[i2][0], (float) u),
+                lerp(cols[i1][1], cols[i2][1], (float) u),
+                lerp(cols[i1][2], cols[i2][2], (float) u),
+                lerp(cols[i1][3], cols[i2][3], (float) u));
     }
 
 }
