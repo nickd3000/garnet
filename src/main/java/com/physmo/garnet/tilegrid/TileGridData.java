492734b167c4ab61357ba668f8a9d6405a65a3db
Nick Donnelly <nickd3000@hotmail.com>
2024-10-30 23:29:32 +0000

    Add tile data array to TileGridData

--- src/main/java/com/physmo/garnet/tilegrid/TileGridData.java
+++ src/main/java/com/physmo/garnet/tilegrid/TileGridData.java
@@ -3,6 +3,7 @@ package com.physmo.garnet.tilegrid;
 public class TileGridData {
     int width, height;
     int[] tileIds;
+    Object[] tileData;
 
     public TileGridData(int width, int height) {
         this.width = width;
@@ -13,11 +14,17 @@ public class TileGridData {
     private void initArrays() {
         int arraySize = width * height;
         tileIds = new int[arraySize];
+        tileData = new Object[arraySize];
     }
 
     public int getTileId(int x, int y) {
         if (x < 0 || y < 0 || x >= width || y >= height) return -1;
         return tileIds[getIndex(x, y)];
+    }
+
+    public Object getTileData(int x, int y) {
+        if (x < 0 || y < 0 || x >= width || y >= height) return null;
+        return tileData[getIndex(x, y)];
     }
 
     private int getIndex(int x, int y) {
@@ -28,4 +35,7 @@ public class TileGridData {
         tileIds[getIndex(x, y)] = val;
     }
 
+    public void setTileData(int x, int y, Object data) {
+        tileData[getIndex(x, y)] = data;
+    }
 }
