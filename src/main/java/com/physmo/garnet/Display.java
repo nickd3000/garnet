19e53457b6d3569a269bc9739ab97eaab20dccd5
Nick Donnelly <nickd3000@hotmail.com>
2024-11-03 13:54:39 +0000

    Add window scaling and centering feature
    Introduced `setWindowScale` method to handle window resizing and centering based on a scaling factor. Refactored `init` to capture primary monitor dimensions for accurate centering during scaling. This enhances flexibility for dynamic window adjustments.

--- src/main/java/com/physmo/garnet/Display.java
+++ src/main/java/com/physmo/garnet/Display.java
@@ -24,6 +24,7 @@ import static org.lwjgl.glfw.GLFW.glfwInit;
 import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
 import static org.lwjgl.glfw.GLFW.glfwSetWindowMonitor;
 import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowSize;
 import static org.lwjgl.glfw.GLFW.glfwSetWindowTitle;
 import static org.lwjgl.glfw.GLFW.glfwShowWindow;
 import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
@@ -43,6 +44,13 @@ public class Display {
     private long windowHandle;
     private final int windowWidth;
     private final int windowHeight;
+    int primaryMonitorWidth;
+    int primaryMonitorHeight;
+
+    double windowScale = 1;
+    int storedWindowX = 0;
+    int storedWindowY = 0;
+
 
     public Display(int windowWidth, int windowHeight) {
         this.windowWidth = windowWidth;
@@ -62,64 +70,6 @@ public class Display {
         return windowHandle;
     }
 
-    public void init() {
-
-        // Setup an error callback. The default implementation
-        // will print the error message in System.err.
-        GLFWErrorCallback.createPrint(System.err).set();
-
-        // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if (!glfwInit())
-            throw new IllegalStateException("Unable to initialize GLFW");
-
-
-        // Configure GLFW
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-        //glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
-
-        // Create the window
-        windowHandle = glfwCreateWindow(windowWidth, windowHeight, "Garnet Framework", NULL, NULL);
-        if (windowHandle == NULL)
-            throw new RuntimeException("Failed to create the GLFW window");
-
-        // Get the thread stack and push a new frame
-        try (MemoryStack stack = stackPush()) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
-
-            // Get the window size passed to glfwCreateWindow
-            glfwGetWindowSize(windowHandle, pWidth, pHeight);
-
-            // Get the resolution of the primary monitor
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            // Center the window
-            assert vidmode != null;
-            glfwSetWindowPos(
-                    windowHandle,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
-        } // the stack frame is popped automatically
-
-        // Make the OpenGL context current
-        glfwMakeContextCurrent(windowHandle);
-
-        // Enable v-sync
-        glfwSwapInterval(1);
-
-        // Make the window visible
-        glfwShowWindow(windowHandle);
-
-        GL.createCapabilities();
-
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(0.0f, windowWidth, windowHeight, 0.0f, 0.0f, 1.0f);
-
-    }
 
     public double[] getWindowToPixelsScale() {
         int[] bufferSize = getWindowSize();
@@ -153,9 +103,6 @@ public class Display {
         glfwSetWindowTitle(windowHandle, title);
     }
 
-    int storedWindowX = 0;
-    int storedWindowY = 0;
-
     public boolean isFullscreen() {
         // Return value will be 0 if in windowed mode.
         return glfwGetWindowMonitor(windowHandle) != 0;
@@ -186,7 +133,75 @@ public class Display {
             System.out.println("Making windowed");
             glfwSetWindowMonitor(windowHandle, 0, storedWindowX, storedWindowY, windowWidth, windowHeight, GLFW_DONT_CARE);
         }
+    }
+
+    public void init() {
+
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
 
 
+        // Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+        //glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Create the window
+        windowHandle = glfwCreateWindow(windowWidth, windowHeight, "Garnet Framework", NULL, NULL);
+        if (windowHandle == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Get the thread stack and push a new frame
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(windowHandle, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            assert videoMode != null;
+            primaryMonitorWidth = videoMode.width();
+            primaryMonitorHeight = videoMode.height();
+
+            glfwSetWindowPos(
+                    windowHandle,
+                    (videoMode.width() - pWidth.get(0)) / 2,
+                    (videoMode.height() - pHeight.get(0)) / 2
+            );
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(windowHandle);
+
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(windowHandle);
+
+        GL.createCapabilities();
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0.0f, windowWidth, windowHeight, 0.0f, 0.0f, 1.0f);
+
+    }
+
+    public void setWindowScale(double windowScale, boolean centerWindow) {
+        this.windowScale = windowScale;
+        glfwSetWindowSize(windowHandle, (int) (windowWidth * windowScale), (int) (windowHeight * windowScale));
+        if (centerWindow) {
+            glfwSetWindowPos(windowHandle, primaryMonitorWidth / 2 - (int) (windowWidth * windowScale / 2), primaryMonitorHeight / 2 - (int) (windowHeight * windowScale / 2));
+        }
     }
 }
