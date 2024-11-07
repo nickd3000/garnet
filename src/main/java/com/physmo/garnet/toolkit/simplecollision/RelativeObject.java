0a6085128aa99e965dead1539b4da3e47c42b1f7
Nick Donnelly <nickd3000@hotmail.com>
2024-11-07 00:36:33 +0000

    Add getters and setters to RelativeObject
    This change introduces getter and setter methods for various attributes in the RelativeObject class, improving encapsulation. It allows controlled access and modification of fields such as distance, originX, originY, and references to Collidable objects.

--- src/main/java/com/physmo/garnet/toolkit/simplecollision/RelativeObject.java
+++ src/main/java/com/physmo/garnet/toolkit/simplecollision/RelativeObject.java
@@ -1,6 +1,62 @@
 package com.physmo.garnet.toolkit.simplecollision;
 
 public class RelativeObject {
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double getOriginX() {
+        return originX;
+    }
+
+    public void setOriginX(double originX) {
+        this.originX = originX;
+    }
+
+    public double getOriginY() {
+        return originY;
+    }
+
+    public void setOriginY(double originY) {
+        this.originY = originY;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
+    }
+
+    public Collidable getOtherObject() {
+        return otherObject;
+    }
+
+    public void setOtherObject(Collidable otherObject) {
+        this.otherObject = otherObject;
+    }
+
+    public Collidable getThisObject() {
+        return thisObject;
+    }
+
+    public void setThisObject(Collidable thisObject) {
+        this.thisObject = thisObject;
+    }
+
     public double distance;
     public double originX;
     public double originY;
