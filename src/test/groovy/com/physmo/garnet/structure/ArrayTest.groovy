cef5af003d7756c45b013a97c312a9cc2a99a6f4
Nick Donnelly <nickd3000@hotmail.com>
2025-01-11 16:57:39 +0000

    Add and enhance Array test cases
    Refactored existing test to use `size()` method properly. Added new test cases to validate array clearing, item addition/retrieval, and iterator functionality to improve test coverage.

--- src/test/groovy/com/physmo/garnet/structure/ArrayTest.groovy
+++ src/test/groovy/com/physmo/garnet/structure/ArrayTest.groovy
@@ -15,8 +15,66 @@ class ArrayTest extends Specification {
             }
 
         then:
-            array.size == 6
+            array.size() == 6
             array.getCapacity() == 10;
 
     }
+
+    def "Clear clears the Array"() {
+        given:
+            Array<String> array = new Array<>(5);
+
+        when:
+            array.add("A")
+            array.add("B")
+            array.add("C")
+            array.add("D")
+            array.clear()
+
+        then:
+            array.size == 0
+
+    }
+
+    def "Array items can be added and retrieved"() {
+        given:
+            Array<String> array = new Array<>(5);
+
+        when:
+            array.add("A")
+            array.add("B")
+            array.add("C")
+
+
+        then:
+            array.get(0) == "A"
+            array.get(1) == "B"
+            array.get(2) == "C"
+
+    }
+
+    def "Array iterator works as expected"() {
+
+        given:
+            Array<String> array = new Array<>(5);
+
+        and:
+            array.add("A")
+            array.add("B")
+            array.add("C")
+
+        when:
+            int count = 0
+            String[] strings = new String[5]
+            for (String str : array) {
+                strings[count++] = str
+            }
+
+        then:
+            strings[0] == "A"
+            strings[1] == "B"
+            strings[2] == "C"
+
+    }
+
 }
